86a9f52ae8b67fb01ba102b07efa42737ffea5ef
techmore <sdolbec1@gmail.com>
2017-09-21 11:58:06 -0400

    Update store.sh

--- store.sh
+++ store.sh
@@ -14,7 +14,7 @@ cd ..
 cat <<EOF >> Dockerfile 
 FROM nginx
 COPY store /usr/share/nginx/html
-RUN apt-get update && apt-get install -y wget 
+RUN apt-get update && apt-get install -y wget apt-utils gnupg2
 RUN echo "deb http://deb.goaccess.io/ $(lsb_release -cs) main" | tee -a /etc/apt/sources.list.d/goaccess.list 
 RUN wget -O - http://deb.goaccess.io/gnugpg.key | apt-key add -
 RUN apt-get update
