53310b2076ceaa4326bb61f03ed42fb517f9f3c9
techmore <sdolbec1@gmail.com>
2017-09-21 11:22:07 -0400

    Update store.sh

--- store.sh
+++ store.sh
@@ -14,12 +14,12 @@ cd ..
 cat <<EOF >> Dockerfile 
 FROM nginx
 COPY store /usr/share/nginx/html
-RUN apt-get update && apt-get install -y \
- echo "deb http://deb.goaccess.io/ $(lsb_release -cs) main" | sudo tee -a /etc/apt/sources.list.d/goaccess.list \
- wget -O - http://deb.goaccess.io/gnugpg.key | sudo apt-key add - \
- apt-get update \
- apt-get install goaccess \
- run goaccess /etc/logs/nginx/access.log -o /usr/share/nginx/html/report.html --real-time-html
+RUN apt-get update && apt-get install -y
+RUN echo "deb http://deb.goaccess.io/ $(lsb_release -cs) main" | sudo tee -a /etc/apt/sources.list.d/goaccess.list 
+RUN wget -O - http://deb.goaccess.io/gnugpg.key | sudo apt-key add -
+RUN apt-get update
+RUN apt-get install goaccess
+RUN goaccess /etc/logs/nginx/access.log -o /usr/share/nginx/html/report.html --real-time-html
 EOF
 
 docker build -t some-content-nginx .
